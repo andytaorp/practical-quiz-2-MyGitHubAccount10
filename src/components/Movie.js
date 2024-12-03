f32de3750afe71be452dc4b7e11f79a151ce57e6
MyGitHubAccount10 <23025668@myrp.edu.sg>
2024-12-03 16:01:21 +0800

    6 passed, 1 failed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,28 +5,44 @@ import React from "react";
  * A single movie item in the movie list, with a checkbox to mark it
  * watched, a text span for the title, and a delete button.
  */
-export default function Movie({movies, onDeleteMovie, onToggledWatched}) {
-    return (
-        <div>
-          <ul>
-            {movies.map((movie) => (
-              <MovieList checkingMovie={movie} key={movie.id} onDeleteMovie={onDeleteMovie} onToggledWatched={onToggledWatched}/>
-            ))}
-          </ul>
-        </div>
-      );
-    }
+// export default function Movie({movies, onDeleteMovie, onToggledWatched}) {
+//     return (
+//         <div>
+//           <ul>
+//             {movies.map((movie) => (
+//               <MovieList checkingMovie={movie} key={movie.id} onDeleteMovie={onDeleteMovie} onToggledWatched={onToggledWatched}/>
+//             ))}
+//           </ul>
+//         </div>
+//       );
+//     }
   
-    function MovieList({checkingMovie, onDeleteMovie, onToggledWatched}) {
-      return (
+//     function MovieList({checkingMovie, onDeleteMovie, onToggledWatched}) {
+//       return (
+//       <li>
+//           <input
+//             type="checkbox"
+//             value={checkingMovie.watched}
+//             onChange={() => onToggledWatched(checkingMovie.id)}
+//           />
+//           <span style = {checkingMovie.watched ? {textDecoration:"line-through"} : {}}> {checkingMovie.title}
+//           </span>
+//           <button onClick={()=>onDeleteMovie(checkingMovie.id)}>❌</button>
+//       </li>);
+// }
+
+export default function Movie({ movie, onToggleWatched, onDeleteMovie }) {
+    return (
       <li>
-          <input
-            type="checkbox"
-            value={checkingMovie.packed}
-            onChange={() => onToggledWatched(checkingMovie.id)}
-          />
-          <span style = {checkingMovie.watched ? {textDecoration:"line-through"} : {}}> {checkingMovie.title}
-          </span>
-          <button onClick={()=>onDeleteMovie(checkingMovie.id)}>❌</button>
-      </li>);
-}
+        <input
+          type="checkbox"
+          checked={movie.watched}
+          onChange={() => onToggleWatched(movie.id)}
+        />
+        <span style={{ textDecoration: movie.watched ? "line-through" : "none" }}>
+          {movie.title}
+        </span>
+        <button onClick={() => onDeleteMovie(movie.id)}>Delete</button>
+      </li>
+    );
+  }
